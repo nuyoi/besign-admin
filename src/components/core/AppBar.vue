0e378b0e71a92ff41898acf0a98d7a59e27a78af
nuyoi <nuyoi@naver.com>
2020-10-13 19:50:38 +0900

    polish(v-app-bar): 페이지 타이틀 및 아이콘 추가

--- src/components/core/AppBar.vue
+++ src/components/core/AppBar.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-app-bar app>
-    <v-toolbar-title>BESIGN ADMIN</v-toolbar-title>
+  <v-app-bar flat color="transparent" app>
+    <v-toolbar-title>PAGE TITLE</v-toolbar-title>
+    <v-spacer />
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-badge overlap dot>
+        <v-icon>mdi-bell-outline</v-icon>
+      </v-badge>
+    </v-btn>
   </v-app-bar>
 </template>
