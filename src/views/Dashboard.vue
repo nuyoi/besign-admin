7ca70db55fa9ba8deafe50faa94fb41279303b9c
nuyoi <nuyoi@naver.com>
2020-10-14 11:36:00 +0900

    polish(Dashboard): 데이터 테이블 추가

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -85,14 +85,12 @@
           </v-card-actions>
         </v-card>
       </v-col>
-    </v-row>
-    <v-row>
       <v-col cols="12" sm="9">
         <v-card>
           <v-sparkline line-width="1" smooth="10" :value="value"></v-sparkline>
         </v-card>
       </v-col>
-      <v-col>
+      <v-col cols="12" sm="3">
         <v-list>
           <v-list-item>
             <v-list-item-content>
@@ -131,6 +129,37 @@
           </v-list-item>
         </v-list>
       </v-col>
+      <v-col cols="12" sm="6">
+        <v-card>
+          <v-card-title>
+            <span>오늘의 인기 콘텐츠</span>
+            <v-spacer />
+            <span>view all</span>
+          </v-card-title>
+          <v-select :items="select" label="운동" dense></v-select>
+          <v-data-table
+            :headers="contentsHeaders"
+            :items="contents"
+            disable-sort
+            hide-default-footer
+          ></v-data-table>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card>
+          <v-card-title>
+            <span>일자별 요약</span>
+            <v-spacer />
+            <span>view all</span>
+          </v-card-title>
+          <v-data-table
+            :headers="summaryHeaders"
+            :items="summary"
+            disable-sort
+            hide-default-footer
+          ></v-data-table>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -139,7 +168,54 @@
 export default {
   name: "Dashboard",
   data: () => ({
-    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]
+    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+    select: ["운동", "식단"],
+    contentsHeaders: [
+      { text: "프로젝트", align: "start", value: "name" },
+      { text: "전문가", value: "trainer" },
+      { text: "조회수", value: "views" },
+      { text: "참가자", value: "join" }
+    ],
+    contents: [
+      {
+        name: "복부 트레이닝 3주 완성",
+        trainer: "홍길동",
+        views: "4238",
+        join: "30"
+      },
+      {
+        name: "하체 단련 프로젝트",
+        trainer: "김건강",
+        views: "1234",
+        join: "10"
+      }
+    ],
+    summaryHeaders: [
+      { text: "일자", align: "start", value: "date" },
+      { text: "페이지뷰", value: "pageView" },
+      { text: "방문자", value: "visit" },
+      { text: "가입", value: "signup" },
+      { text: "댓글", value: "comment" },
+      { text: "리뷰", value: "review" }
+    ],
+    summary: [
+      {
+        date: "2020-10-12",
+        pageView: "828",
+        visit: "85",
+        signup: "10",
+        comment: "23",
+        review: "9"
+      },
+      {
+        date: "2020-10-11",
+        pageView: "452",
+        visit: "57",
+        signup: "7",
+        comment: "17",
+        review: "5"
+      }
+    ]
   })
 };
 </script>
